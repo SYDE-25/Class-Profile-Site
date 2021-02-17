0d2d2342fae031df513fee72a86cea7102640844
Shivam Sharma <shivam.sharm@icloud.com>
2021-02-16 22:27:06 -0500

    Implement basic three.js canvas

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <canvas id="threejs-nodes" className={styles.canvas}></canvas>
       <div className={styles.container}>
         <banner className={styles.banner}>
           <h1 className={styles.title}>Welcome to our class profile</h1>
 
           <p className={styles.description}>Keep scrolling to see more</p>
         </banner>
+
+        <script type="module" src="/nodes.js"></script>
 
         <div className={styles.content}>
           <h1>More Content Here</h1>
---
+++ public/nodes.js
@@ -0,0 +1,46 @@
+import * as THREE from "./three.module.js";
+
+let camera, scene, renderer;
+let geometry, material, mesh;
+
+init();
+
+function init() {
+    const canvas = document.querySelector('#threejs-nodes');
+	renderer = new THREE.WebGLRenderer( {canvas: canvas, alpha: true, antialias: true} );
+	renderer.setSize( window.innerWidth, window.innerHeight * 0.7 );
+	renderer.setAnimationLoop( animation );
+
+    // Creating the camera
+    const fov = 75;
+    const aspect = 2;
+    const near = 0.1;
+    const far = 5;
+    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 2;
+
+    // Create a scene
+    scene = new THREE.Scene();
+
+    // Making a box geometry
+    const boxWidth = 1;
+    const boxHeight = 1;
+    const boxDepth = 1;
+    geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+    // Declaring a material
+    material = new THREE.MeshNormalMaterial();
+
+    // Making a mesh
+    mesh = new THREE.Mesh(geometry, material);
+
+    // Add it to the scene
+    scene.add(mesh);
+}
+
+function animation( time ) {
+	mesh.rotation.x = time / 2000;
+	mesh.rotation.y = time / 1000;
+
+	renderer.render( scene, camera );
+}
