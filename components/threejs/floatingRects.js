e6244a02c59ac5710c3a14d8f536687c153cb32e
Shivam Sharma <shivam.sharm@icloud.com>
2021-03-31 18:20:01 -0400

    Refine background implementation

--- components/threejs/floatingRects.js
+++ components/threejs/floatingRects.js
@@ -1,28 +1,108 @@
-import { FloatyRect } from "./floatyRect";
-import { Canvas } from 'react-three-fiber'
+import { useState } from 'react';
+import { Canvas, useFrame, useThree } from 'react-three-fiber';
 
-export default function FloatingRects() {
+export default function Default(props) {
   return (
     <Canvas orthographic gl={{ alpha: true, antialias: true }}>
       <ambientLight />
-      <FloatyRect
-        relativePosition={[0.75, 0.55]}
-        relativeSize={[1.5, 1.1]}
-        rotation={[0, 0, 0.2]}
-        mass={90}
-      />
-      <FloatyRect
-        relativePosition={[0.65, 0.65]}
-        relativeSize={[0.7, 0.7]}
-        rotation={[0, 0, 0.5]}
-        mass={60}
-      />
-      <FloatyRect
-        relativePosition={[0.75, 0.75]}
-        relativeSize={[0.4, 0.4]}
-        rotation={[0, 0, 0.15]}
-        mass={30}
+      <FloatingRects
+        rects={[
+          {
+            relativePosition: [0.8, 0.5],
+            relativeSize: [1.5, 1.2],
+            rotation: [0, 0, 0.2],
+            mass: 90,
+          },
+          {
+            relativePosition: [0.7, 0.7],
+            relativeSize: [0.9, 0.9],
+            rotation: [0, 0, 0.5],
+            mass: 40,
+          },
+          {
+            relativePosition: [0.75, 0.75],
+            relativeSize: [0.5, 0.5],
+            rotation: [0, 0, 0.15],
+            mass: 60,
+          },
+        ]}
       />
     </Canvas>
   );
 }
+
+function FloatingRects(props) {
+  const { mouse, viewport } = useThree();
+
+  const [prepRect] = useState(() => (rect) => {
+    rect.size = [
+      rect.relativeSize[0] * viewport.width,
+      rect.relativeSize[1] * viewport.height,
+    ];
+  });
+
+  const [animationTime, setAnimationTime] = useState(0);
+  const [baseRects] = useState(props.rects);
+  const [currentRects] = useState(props.rects);
+
+  useFrame(({ mouse }) => {
+    baseRects.forEach((rect, i) => {
+      if (animationTime == 0) {
+        currentRects[i].currentOffset = [0, 0];
+        currentRects[i].targetOffset = [0, 0];
+      }
+      if (animationTime % 5 == 0) {
+        currentRects[i].targetOffset = [
+          (mouse.x * 3000) / rect.mass,
+          (mouse.y * 3000) / rect.mass,
+        ];
+      }
+
+      const distance = [
+        currentRects[i].targetOffset[0] - currentRects[i].currentOffset[0],
+        currentRects[i].targetOffset[1] - currentRects[i].currentOffset[1],
+      ];
+
+      currentRects[i].currentOffset = [
+        currentRects[i].currentOffset[0] + distance[0] / rect.mass,
+        currentRects[i].currentOffset[1] + distance[1] / rect.mass,
+      ];
+
+      currentRects[i].position = [
+        -(viewport.width / 2) +
+          rect.relativePosition[0] * viewport.width +
+          currentRects[i].currentOffset[0],
+        viewport.height / 2 -
+          rect.relativePosition[1] * viewport.height +
+          currentRects[i].currentOffset[1],
+      ];
+
+      currentRects[i].size = [
+        rect.relativeSize[0] * viewport.width,
+        rect.relativeSize[1] * viewport.height,
+      ];
+    });
+    setAnimationTime(animationTime + 1);
+  });
+
+  return (
+    <>
+      {currentRects.map((rect) => {
+        return (
+          <mesh position={rect.position} rotation={rect.rotation}>
+            <planeGeometry args={rect.size} />
+            <meshStandardMaterial
+              color={'white'}
+              opacity={0.06}
+              transparent={true}
+            />
+          </mesh>
+        );
+      })}
+    </>
+  );
+}
+
+Number.prototype.map = function (in_min, in_max, out_min, out_max) {
+  return ((this - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Head from "next/head";
-import Header from "../components/header";
-import styles from "../styles/Home.module.scss";
+import Head from 'next/head';
+import Header from '../components/header';
+import styles from '../styles/Home.module.scss';
 
-import FloatingRects from '../components/threejs/floatingRects';
+import Background from '../components/threejs/floatingRects';
 
 export default function Home() {
   return (
@@ -16,11 +16,13 @@ export default function Home() {
 
       <div className={styles.banner}>
         <h1 className={styles.title}>Welcome to our class profile</h1>
-        <h4 className={styles.description}>Head over to the profile page to see more</h4>
+        <h4 className={styles.description}>
+          Head over to the profile page to see more
+        </h4>
       </div>
 
       <div className={styles.background}>
-        <FloatingRects />
+        <Background />
       </div>
     </>
   );
