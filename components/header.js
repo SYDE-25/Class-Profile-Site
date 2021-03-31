66f39fab74178672ea57650b1da51234709a55b6
Parmin <parmin@Parmins-MacBook-Pro.local>
2021-03-30 22:23:00 -0400

    implemented the firebase sdk and implemented the templete to retrive data from firestore

--- components/header.js
+++ components/header.js
@@ -2,15 +2,32 @@ import Link from "next/link";
 import SiteLogo from "./siteLogo";
 
 import styles from "./header.module.scss";
+import { db } from "../firebaseConfig";
 
 export default function Header() {
+    /*
+    ***BASIC WAY TO GET DATA FROM FIRESTORE TEMPLETE***
+    
+  const backup = db.collection("1A Data").doc("Backup");
+
+  async function anything() {
+    const doc = await backup.get();
+    if (!doc.exists) {
+      console.log("No such document!");
+    } else {
+      console.log("Document data:", doc.data());
+    }
+  }
+  anything();
+
+  */
   return (
     <div className={styles.header}>
-      <Link href="/">
+      <Link href = "/">
         <a>
-          <SiteLogo className={styles.siteLogo}/>
+          <SiteLogo className={styles.siteLogo} />
         </a>
-      </Link>
+        </Link>
     </div>
   );
 }
