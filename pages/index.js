c8037ea4b96920baaec9c5ddb7c0eb261e90daf7
Parmin <parmin@Parmins-MacBook-Pro.local>
2021-04-03 17:01:33 -0400

    merge conflicts

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import Header from '../components/header';
 import styles from '../styles/Home.module.scss';
-
+import Link from  "next/link";
 import Background from '../components/threejs/floatingRects';
+
 
 export default function Home() {
   return (
@@ -23,6 +24,34 @@ export default function Home() {
 
       <div className={styles.background}>
         <Background />
+        <div className={styles.content}>
+          <h1>More Content Here</h1>
+          
+          <Link href = "./cards/demographics/Demographics">
+              Demographics
+          </Link>
+          
+          <Link href = "./cards/highschool/Highschool">
+            Highschool
+          </Link>
+
+          <Link href = "./cards/coop/Coop">
+              Coop
+          </Link>
+
+          <Link href = "./cards/lifestyle/Lifestyle">
+              Lifestyle
+          </Link>
+
+          <Link href = "./cards/academics/Academics">
+              Academics
+          </Link>
+
+          <Link href = "./cards/igor/Igor">
+              Igor
+          </Link>
+
+        </div>
       </div>
     </>
   );
