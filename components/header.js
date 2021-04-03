559de4ecd18d6c61d07558fb3b5802287ec3eac2
Parmin <parmin@Parmins-MacBook-Pro.local>
2021-04-03 16:21:47 -0400

    added a txt file for firebase template

--- components/header.js
+++ components/header.js
@@ -5,22 +5,7 @@ import styles from "./header.module.scss";
 import { db } from "../firebaseConfig";
 
 export default function Header() {
-    /*
-    ***BASIC WAY TO GET DATA FROM FIRESTORE TEMPLETE***
-    
-  const backup = db.collection("1A Data").doc("Backup");
-
-  async function anything() {
-    const doc = await backup.get();
-    if (!doc.exists) {
-      console.log("No such document!");
-    } else {
-      console.log("Document data:", doc.data());
-    }
-  }
-  anything();
-
-  */
+   
   return (
     <div className={styles.header}>
       <Link href = "/">
