27032e986590f66b54adced4a19f27da4c6238f8
shivam-sh <shivam.sharm@icloud.com>
2021-04-11 16:54:49 -0400

    Rebase on develop
    Co-Authored-By: Evan W. <71536798+eevanwong@users.noreply.github.com>

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,23 +1,26 @@
 import Link from 'next/link';
-
 import SiteLogo from '../siteLogo';
 import styles from './layout.module.scss';
 
 export function Header() {
-    return (
-      <div className={styles.header}>
-        <Link href="/">
-          <a>
-            <SiteLogo className={styles.siteLogo} />
-          </a>
-        </Link>
-  
+  return (
+    <div className={styles.header}>
+      <Link href="/">
+        <SiteLogo className={styles.siteLogo} />
+      </Link>
+
+      <div className={styles.divLinks}>
         <Link href="/profile">
-            <a className={styles.navLink}>Profile</a>
+          <a className={styles.navLink}>profile</a>
+        </Link>
+
+        <Link href="https://www.youtube.com/watch?v=S0yLbdZRu3A&ab_channel=ElCroissant">
+          <a className={styles.navLink}>about</a>
         </Link>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export function Content(props) {
   return (
