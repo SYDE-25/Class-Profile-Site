ade01494f8fb7ef0a6cb65c6a2d76b2597fcdb4d
YaybeNo <evanmatt.wong@gmail.com>
2021-04-12 20:18:47 -0400

    forgot merge conflict in academics

--- pages/profile/academics/index.js
+++ pages/profile/academics/index.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import Head from 'next/head';
-import React from "react";
-import Content from "../../../components/layout/layout"
-import PieChart from "../../../components/charts/pieChart";
-=======
 import Head from "next/head";
 import React from "react";
 import { Content, Header } from "../../../components/layout/layout";
 import DoughnutChart from "../../../components/charts/doughnut";
->>>>>>> develop
 
 export default function Academics() {
   
@@ -18,17 +11,11 @@ export default function Academics() {
         <title>Academics • Class Profile</title>
       </Head>
 
-<<<<<<< HEAD
-      <Content>
-          <h1>Academics</h1>
-          <PieChart datatype = 'Backup' />
-=======
       <Header />
 
       <Content>
         <h1>Academics</h1>
         <DoughnutChart datatype="Hometown" />
->>>>>>> develop
       </Content>
     </>
   );
