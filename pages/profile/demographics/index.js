b4b096dbe614408cc2aaab103b487fb45438909c
Parmin <parmin@Parmins-MacBook-Pro.local>
2021-04-12 23:20:21 -0400

    changed file name

--- pages/profile/demographics/index.js
+++ pages/profile/demographics/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { Content, Header } from "../../../components/layout/layout";
 import DoughnutChart from "../../../components/charts/doughnut";
 
-import { text } from "../../../data";
+import { text } from "../../../text";
 export default function Demographics() {
   return (
     <>
