1831058a334994738de09a709d68df12a7e50f2d
Parmin <parmin@Parmins-MacBook-Pro.local>
2021-04-12 23:17:28 -0400

    made the basic data file where the data for writeup will be stored

---
+++ data.js
@@ -0,0 +1,17 @@
+module.exports = {
+  text: {
+    BACKUP_TEXT: "",
+    COOP_ROUND_TEXT: "",
+    COOP_USEFULNESS_VS_DIFFICULTY_TEXT: "",
+    GENDER_TEXT: "",
+    HS_AVERAGE_TEXT: " ",
+    HS_AVERAGE_VS_UNI_TEXT: "",
+    HOMETOWN_TEXT: "",
+    PARENTAL_EDUCATION_TEXT: " ",
+    PARENTS_BORN_TEXT: "",
+    PARENTS_STEM_TEXT: "",
+    RELIGION_TEXT: "",
+    SEXUALITY_TEXT: "",
+    YEAR_BORN_TEXT: "",
+  },
+};
--- pages/profile/demographics/index.js
+++ pages/profile/demographics/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { Content, Header } from "../../../components/layout/layout";
 import DoughnutChart from "../../../components/charts/doughnut";
 
+import { text } from "../../../data";
 export default function Demographics() {
   return (
     <>
