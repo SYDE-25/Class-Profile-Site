0e608e45e1dd3cf3b5aa4b6f72bddacc76737005
Evan W <71536798+eevanwong@users.noreply.github.com>
2021-04-13 14:18:45 -0400

    Update index.js

--- pages/profile/demographics/index.js
+++ pages/profile/demographics/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import { Content, Header } from "../../../components/layout/layout";
 import DoughnutChart from "../../../components/charts/doughnut";
+import { text } from "../../../text1A";
 
-import { text } from "../../../text";
 export default function Demographics() {
   return (
     <>
