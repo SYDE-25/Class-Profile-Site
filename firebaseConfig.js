1b4a2491c7c3e6b901e458ab3a587f669820d457
Shivam Sharma <shivam.sharm@icloud.com>
2021-04-16 13:48:49 -0400

    Fix firebase config typo

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,7 +6,7 @@ if (!Firebase.apps.length) {
         authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         storageBucket: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINIG_SENDER_ID,
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
         appId: process.env.NEXT_PUBLIC_FIREBASE_APPID
     });
  }
