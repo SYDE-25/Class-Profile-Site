227b44eb613ee68f342f86487844ef28c45bbf8e
Hannah Hepburn <71160884+HMHepburn@users.noreply.github.com>
2021-04-18 16:48:44 -0400

    Card Edits Part 2
    HTML components for cards

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,8 +1,16 @@
+<<<<<<< HEAD
 import Head from "next/head";
 import Link from "next/link";
 
 import { Content, Header } from "../../components/layout/layout";
 import styles from "./profilePage.module.scss";
+=======
+import Head from 'next/head';
+import Link from 'next/link';
+
+import Header from '../../components/header/header';
+import styles from './profilePage.module.scss';
+>>>>>>> 5043f0c... Card Edits Part 2
 
 export default function Profile() {
   return (
@@ -17,6 +25,7 @@ export default function Profile() {
         <h1>Profile</h1>
         <div className={styles.card}>
           <Link href="profile/demographics">
+<<<<<<< HEAD
             <div className={styles.demographics}>
               <h3>Demographics</h3>
             </div>
@@ -45,6 +54,24 @@ export default function Profile() {
             <div className={styles.igor}>
               <h3>Igor</h3>
             </div>
+=======
+            <div className={styles.demographics}><h3>Demographics</h3></div>
+          </Link>
+          <Link href="profile/high-school">
+            <div className={styles.academics}><h3>Academics</h3></div>
+          </Link>
+          <Link href="profile/co-op">
+            <div className={styles.coop}><h3>Co-op</h3></div>
+          </Link>
+          <Link href="profile/lifestyle">
+            <div className={styles.lifestyle}><h3>Lifestyle</h3></div>
+          </Link>
+          <Link href="profile/academics">
+            <div className={styles.highschool}><h3>High School</h3></div>
+          </Link>
+          <Link href="profile/igor">
+            <div className={styles.igor}><h3>Igor</h3></div>
+>>>>>>> 5043f0c... Card Edits Part 2
           </Link>
         </div>
       </div>
