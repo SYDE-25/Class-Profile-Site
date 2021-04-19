5e249b5ef4ab9cf30b9983d4e220e8faff68ae79
YaybeNo <evanmatt.wong@gmail.com>
2021-04-19 18:03:34 -0400

    added beginning component to each section + segment takes in multiple graphs

--- pages/profile/demographics/index.js
+++ pages/profile/demographics/index.js
@@ -3,6 +3,7 @@ import { Content, Header } from "../../../components/layout/layout";
 import DoughnutChart from "../../../components/charts/doughnut";
 import Segment from "../../../components/layout/segment/segment"
 import { text } from "../../../text1A";
+import SectionTitle from "../../../components/layout/sectiontitle/sectiontitle";
 
 export default function Demographics() {
   return (
@@ -15,12 +16,11 @@ export default function Demographics() {
 
       <Content>
         <h1>Demographics</h1>
-        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
-          
-        <br/>  
-        <br/>  
-        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </h4>
+
+        <SectionTitle>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </SectionTitle>
 
         <Segment title="Gender" graph={<DoughnutChart datatype="Gender" />}>
         "At vero eos et accusamus et iusto odio dignissimos ducimus q
@@ -30,14 +30,19 @@ export default function Demographics() {
         </Segment>
         
         <Segment title="Parents" graph={<DoughnutChart datatype="Parents Born" />}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+
+        </Segment>
+        <Segment graph={
+        <>
+        <DoughnutChart datatype="Parental Education"/>
+        <DoughnutChart datatype="Parents Stem" />
+        </>
+        } onTop={true}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
           
         </Segment>
-        <Segment graph={<DoughnutChart datatype="Parental Education" />}>
-          
-        </Segment>
-        <Segment  graph={<DoughnutChart datatype="Parents Stem" />}>
-          
-        </Segment>
+
         <Segment title="Religion" graph={<DoughnutChart datatype="Religion" />}>
           
         </Segment>
