e936ff2a5dda236e9c6dc273f7544a17af227c77
Parmin <parmin@Parmins-MacBook-Pro.local>
2021-04-19 15:07:30 -0400

    clean up code

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,8 +1,8 @@
-import Head from 'next/head';
-import Link from 'next/link';
+import Head from "next/head";
+import Link from "next/link";
 
-import Header from '../../components/header/header';
-import styles from './profilePage.module.scss';
+import { Content, Header } from "../../components/layout/layout";
+import styles from "./profilePage.module.scss";
 
 export default function Profile() {
   return (
@@ -17,22 +17,34 @@ export default function Profile() {
         <h1>Profile</h1>
         <div className={styles.card}>
           <Link href="profile/demographics">
-            <div className={styles.demographics}><h3>Demographics</h3></div>
+            <div className={styles.demographics}>
+              <h3>Demographics</h3>
+            </div>
           </Link>
           <Link href="profile/high-school">
-            <div className={styles.academics}><h3>Academics</h3></div>
+            <div className={styles.academics}>
+              <h3>Academics</h3>
+            </div>
           </Link>
           <Link href="profile/co-op">
-            <div className={styles.coop}><h3>Co-op</h3></div>
+            <div className={styles.coop}>
+              <h3>Co-op</h3>
+            </div>
           </Link>
           <Link href="profile/lifestyle">
-            <div className={styles.lifestyle}><h3>Lifestyle</h3></div>
+            <div className={styles.lifestyle}>
+              <h3>Lifestyle</h3>
+            </div>
           </Link>
           <Link href="profile/academics">
-            <div className={styles.highschool}><h3>High School</h3></div>
+            <div className={styles.highschool}>
+              <h3>High School</h3>
+            </div>
           </Link>
           <Link href="profile/igor">
-            <div className={styles.igor}><h3>Igor</h3></div>
+            <div className={styles.igor}>
+              <h3>Igor</h3>
+            </div>
           </Link>
         </div>
       </div>
