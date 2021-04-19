63e94c28beb777e5006849918fb5a86a36d7deb6
Ebrahim Yaakoub Agha <74978422+ebrahim02-ag@users.noreply.github.com>
2021-04-19 00:15:04 -0400

    adding bar graph

---
+++ components/charts/bar.js
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { db } from "../../firebaseConfig";
+import { Bar } from "react-chartjs-2";
+
+export default function BarGraph(props) {
+  const [data, setData] = useState({
+    val: [],
+    label: [],
+    color: [],
+  });
+
+  db.collection("1A Data")
+    .doc(props.datatype)
+    .onSnapshot(
+      (snapshot) => {
+        let data = {
+          val: [],
+          label: [],
+          color: [],
+          title: "",
+        };
+
+        snapshot.data().x.forEach((element) => {
+          data.val.push(element.value);
+          data.label.push(element.index);
+          data.color.push(element.color);
+        });
+        data.title = snapshot.data().title;
+        setData(data);
+      },
+      (err) => {
+        console.log("Error fetching firebase snapshot! " + err);
+      }
+    );
+
+  return (
+    <div>
+      {/* <div className="header">
+        <h1 className="title">Doughnut</h1>
+      </div> */}
+      <div className="chart">
+        <Bar
+          data={{
+            labels: data.label,
+            datasets: [
+              {
+                label: "# of Students",
+                data: data.val,
+                backgroundColor: data.color,
+                borderColor: data.color,
+                borderWidth: 1,
+              },
+            ],
+          }}
+          options={{
+            responsive: true,
+            maintainAspectRatio: true,
+            legend: { display: false },
+            title: {
+              display: true,
+              text: data.title,
+              fontColor: "#ffffff",
+            },
+            scales: {
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero: true
+                      }
+                  }]
+              }
+          }
+          }
+          height={props.height ? props.height : "100%"}
+          width={props.width ? props.width : "100%"}
+        />
+      </div>
+    </div>
+  );
+}
