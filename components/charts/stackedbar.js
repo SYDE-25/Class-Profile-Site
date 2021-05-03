1d670eeaca6a4556ceabc89ebad14b6aea2f7a66
Ebrahim Yaakoub Agha <74978422+ebrahim02-ag@users.noreply.github.com>
2021-05-03 05:22:16 -0400

    added percentage to hover

--- components/charts/stackedbar.js
+++ components/charts/stackedbar.js
@@ -97,6 +97,15 @@ export default function StackedBar(props) {
           options={{
             responsive: true,
             maintainAspectRatio: true,
+            tooltips: {
+              enabled: true, 
+              callbacks: {
+                label: function(tooltipItem) { 
+                    var label = data.label[tooltipItem.datasetIndex];
+                    return label + ': ' +  '%' + tooltipItem.value ;
+                }
+              }
+            },
             legend: { 
               display: true,
               reverse: true, 
